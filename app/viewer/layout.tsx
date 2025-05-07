3d72fa1bd3d21c78be524b141938168afc4ed7e4
Oluwadarasimi <dara.adedigba@borro.me>
2025-05-07 12:58:09 +0100

    chore: move viewer component to root app path

--- app/viewer/layout.tsx
+++
@@ -1,6 +0,0 @@
-import ViewerLayout from '@/components/layout/viewer/layout';
-import { LayoutProps } from '@/lib/types/layout';
-
-export default function Layout(props: LayoutProps) {
-	return <ViewerLayout {...props} />;
-}
